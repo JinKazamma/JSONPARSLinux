28ff1fdb46dfd0494e63d57b97d79d034b4b1df2
JinKazamma <gerttyyy@gmail.com>
2023-03-06 01:06:27 +0500

    pereimPeremennih

--- main.cpp
+++ main.cpp
@@ -2,85 +2,85 @@
 using namespace std;
 int main()
 {
-	string stroka;
-	string stroka2;
-	int temp = 0;
-	int temp2 = 0;
-	vector <Massiv*> mass1;
-	vector <Massiv*> mass2;
-	vector <Massiv*> mass3;
-	vector <Massiv*> mass4;
-	vector <Massiv*> mass5;
-	Curl(stroka);
-	Curl(stroka2);
-	Fortemp(stroka, temp);
-	Fortemp(stroka2, temp2);
-	Jonson(stroka, mass1, temp);
-	Jonson(stroka2, mass2, temp2);
-	cout << temp << endl;
-	cout << temp2 << endl;
-	/*for (int i = 0; i < mass1.size(); i++)
+	string adress;
+	string adress2;
+	int kolichestvoElementov = 0;
+	int kolichestvoElementov2 = 0;
+	vector <Massiv*> objectDannyh1;
+	vector <Massiv*> objectDannyh2;
+	vector <Massiv*> dannie1NoNet2;
+	vector <Massiv*> dannie2NoNet1;
+	vector <Massiv*> versionReleasEstV1bolshe2;
+	Curl(adress);
+	Curl(adress2);
+	Fortemp(adress, kolichestvoElementov);
+	Fortemp(adress2, kolichestvoElementov2);
+	Jonson(adress, objectDannyh1, kolichestvoElementov);
+	Jonson(adress2, objectDannyh2, kolichestvoElementov2);
+	for (int i = 0; i < objectDannyh1.size(); i++)
 	{
-		for (int j = 0; j < mass2.size(); j++)
+		for (int j = 0; j < objectDannyh2.size(); j++)
 		{
-			if (*mass1[i] == *mass2[j])
+			if (*objectDannyh1[i] == *objectDannyh2[j])
 			{
 				break;
 			}
-			if (j == mass2.size() - 1)
+			if (j == objectDannyh2.size() - 1)
 			{
-				mass3.push_back(new Massiv(*mass1[i]));
+				dannie1NoNet2.push_back(new Massiv(*objectDannyh1[i]));
 			}
 		}
 	}
-	for (int i = 0; i < mass2.size(); i++)
+	for (int i = 0; i < objectDannyh2.size(); i++)
 	{
-		for (int j = 0; j < mass1.size(); j++)
+		for (int j = 0; j < objectDannyh1.size(); j++)
 		{
-			if (*mass2[i] == *mass1[j])
+			if (*objectDannyh2[i] == *objectDannyh1[j])
 			{
 				break;
 			}
-			if (j == mass1.size() - 1)
+			if (j == objectDannyh1.size() - 1)
 			{
-				mass4.push_back(new Massiv(*mass2[i]));
+				dannie2NoNet1.push_back(new Massiv(*objectDannyh2[i]));
 			}
 		}
-	}*/
-	for (int i = 0; i < mass1.size(); i++)
+	}
+	for (int i = 0; i < objectDannyh1.size(); i++)
 	{
-		for (int j = 0; j < mass2.size(); j++)
+		for (int j = 0; j < objectDannyh2.size(); j++)
 		{
-			if (mass1[i]->Ifsmall(*mass2[j]))
+			if (objectDannyh1[i]->Ifsmall(*objectDannyh2[j]))
 			{
 				break;
 			}		
-			if (j == mass2.size() - 1)
+			if (j == objectDannyh2.size() - 1)
 			{
-				mass5.push_back(new Massiv(*mass1[i]));
+				versionReleasEstV1bolshe2.push_back(new Massiv(*objectDannyh1[i]));
 			}
 		}
 	}
-	/*cout<<"{\"Packeg 1 has\":[";
-	for (int i = 0; i < mass3.size(); i++)
+	cout<<"{\"Packeg 1 has\":[";
+	for (int i = 0; i < dannie1NoNet2.size(); i++)
 	{
-		mass3[i]->putdata();
-		if (i!=mass3.size()-1)
+		dannie1NoNet2[i]->putdata();
+		if (i!=dannie1NoNet2.size()-1)
 		cout<<", ";
 	}cout<<"]}";
 	cout<<"{\"Packeg 2 has\":[";
-	for (int i = 0; i < mass4.size(); i++)
+	for (int i = 0; i < dannie2NoNet1.size(); i++)
 	{
-		mass4[i]->putdata();
-		if (i!=mass4.size()-1)
-		cout<<", ";
-	}cout<<"]}";*/
-	cout<<"{\"version releas 1 bigger\":[";
-	for (int i = 0; i < mass5.size(); i++)
-	{
-		mass5[i]->putdata();
-		if (i!=mass5.size()-1)
+		dannie2NoNet1[i]->putdata();
+		if (i!=dannie2NoNet1.size()-1)
 		cout<<", ";
 	}cout<<"]}";
+	cout<<"{\"version releas 1 bigger\":[";
+	for (int i = 0; i < versionReleasEstV1bolshe2.size(); i++)
+	{
+		versionReleasEstV1bolshe2[i]->putdata();
+		if (i!=versionReleasEstV1bolshe2.size()-1)
+		cout<<", ";
+	}cout<<"]}"<<endl;
+
+
 	return 0;
 }
